38f6af7d1e88643bab6d5e5aa935c7c4958341b6
Pikachu747 <77072236+Pikachu747@users.noreply.github.com>
2025-06-05 17:06:46 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,36 +1,43 @@
-console.log("welcome to tic tac toe");
-let turn= "x"
-const changeTurn = () =>{
-    return turn ==="x"? "0" :"x"
-}
+console.log("Welcome to Tic Tac Toe");
 
-const checkWin =()=>{
-    let spantext = document.getElementsByClassName('spantext');
-    let wins
-    [
-        [0,1,2]
-        [3,4,5]
-        [6,7,8]
-        [0,3,6]
-        [1,4,7]
-        [2,5,8]
-        [0,4,8]
-        [2,4,6]
-    ]
-    wins.forEach(e => 
-        {
-             if((spantext[e[0]].innerHTML === spantext[e[1]].innerHTML ) && (spantext[e[1].innerHTML] === spantext[e[2]].innerHTML === spantext[e[1]].innerHTML ) && (spantext [e[0]] !==" ")) { "won"}
-        })
-}
+let turn = "X";
+let isGameOver = false;
 
-let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(Element => {
-    let spantext =Element.querySelector('spantext');
-    Element.addEventListener('click',() =>{
-        if(spantext.innerHTML ==='  '){
-            spantext.innerHTML = turn ();
-            turn = changeTurn();
-            checkWin();
-        }
-    })
-})
+const changeTurn = () => (turn === "X" ? "O" : "X");
+
+const checkWin = () => {
+  const spantext = document.getElementsByClassName("spantext");
+  const wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  wins.forEach((e) => {
+    if (
+      spantext[e[0]].innerText !== "" &&
+      spantext[e[0]].innerText === spantext[e[1]].innerText &&
+      spantext[e[1]].innerText === spantext[e[2]].innerText
+    ) {
+      alert(spantext[e[0]].innerText + " has won!");
+      isGameOver = true;
+    }
+  });
+};
+
+const boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach((element) => {
+  const spantext = element.querySelector(".spantext");
+  element.addEventListener("click", () => {
+    if (spantext.innerText === "" && !isGameOver) {
+      spantext.innerText = turn;
+      checkWin();
+      if (!isGameOver) turn = changeTurn();
+    }
+  });
+});
